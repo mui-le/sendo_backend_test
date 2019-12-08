a2629b551998c8d9054e2fcd97b392038dd3920d
legolas-le <ngonthapvinhhang@gmail.com>
2019-12-08 16:29:02 +0700

    [routes] update routes

--- app/routes.py
+++ app/routes.py
@@ -16,7 +16,7 @@ def index():
 def get_vouchers():
     """Get all new vouchers"""
     vouchers = Voucher.get_all()
-    result = jsonify(vouchers=[e.serialize() for e in vouchers])
+    result   = jsonify(vouchers=[e.serialize() for e in vouchers])
     return make_response(result)
 
 @app.route('/api/v1.0/vouchers', methods=['POST'])
@@ -38,15 +38,39 @@ def create_voucher():
 
 @app.route('/api/v1.0/vouchers/<int:voucher_id>')
 def get_voucher(voucher_id):
-    """get voucher"""
+    """get voucher detail"""
+    voucher = Voucher.get(voucher_id).first_or_404()
+    return make_response(jsonify(voucher=voucher.serialize()))
 
 @app.route('/api/v1.0/vouchers/<int:voucher_id>', methods=['PUT'])
 def update_voucher(voucher_id):
     """updating voucher"""
+    voucher = Voucher.query.filter_by(id=voucher_id).first_or_404()
+    data = request.get_json(silent=True)
+    if "start" in data:
+        voucher.start = datetime.strptime(data['start'], '%Y-%m-%d %H:%M:%S')
+    if 'end' in data:
+        voucher.end = datetime.strptime(data['end'], '%Y-%m-%d %H:%M:%S')
+    if 'code' in data:
+        voucher.code = data['code']
+    if 'value' in data:
+        voucher.value = data['value']
+
+    if voucher.is_overlap_with_other() or voucher.start >= voucher.end:
+        abort(400)
+
+    db.session.commit()
+    return make_response(jsonify(voucher=voucher.serialize()), 201)
 
 @app.route('/api/v1.0/vouchers/<int:voucher_id>', methods=['DELETE'])
 def delete_voucher(voucher_id):
-    """deleting voucher"""
+    """delete voucher"""
+    voucher = Voucher.get(voucher_id).first_or_404()
+    if len(voucher) == 0:
+        abort(404)
+    db.session.delete(voucher)
+    db.session.commit()
+    return make_response(jsonify({'result': True}))
 
 @app.errorhandler(404)
 def not_found(error):
